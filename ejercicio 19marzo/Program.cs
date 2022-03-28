9fc25ab081fd71f8fff555c5bfd4e218c21aadb9
DELL <DELL@DELL>
2022-03-27 18:57:45 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio 19marzo/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using ejercicio_19marzo.clase;
+
+Carro SamuelCarrito = new Carro("Honda Civic", 2022, "Negro", 150);
+
+
+Console.WriteLine($"El carro de Samuel es un {SamuelCarrito.Marca}");
+Console.WriteLine($" Modelo {SamuelCarrito.Modelo}");
+Console.WriteLine($" Color {SamuelCarrito.Color}\n");
+//Console.WriteLine($"Samuel's model car is {SamuelCarrito.Modelo}");
+
+
+//ENCENDER EL MOTOR
+
+string mensaje;
+Console.WriteLine(SamuelCarrito.GetVelocidadActual());
+mensaje=SamuelCarrito.EncenderMotor();
+Console.WriteLine(mensaje);
+
+
+for (int i = 0; i < 5; i++)
+{
+    mensaje=SamuelCarrito.acelerar();
+    Console.WriteLine(mensaje);
+}
+for (int i = 0; i < 6; i++)
+{
+    mensaje=SamuelCarrito.Desacelerar();
+    Console.WriteLine(mensaje);
+
+}
+
+
+//EL FRENO
+//mensaje=SamuelCarrito.Frenado();
+//Console.WriteLine(mensaje)
+
+
+//APAGAR EL MOTOR
+mensaje =SamuelCarrito.ApagarMotor();
+Console.WriteLine(mensaje);
+
+   mensaje = SamuelCarrito.pitar();
+    Console.WriteLine(mensaje);
+
+for (int i = 0; i < 3; i++)
+{
+
+    mensaje = SamuelCarrito.pitar();
+    Console.WriteLine(mensaje);
+
+
+}
---
+++ ejercicio 19marzo/clase/carro.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_19marzo.clase
+{
+    internal class Carro
+    {
+
+
+        public string Marca { get; }
+        public int Modelo { get; }
+
+        public String Color { get; set; }
+        private int bocina = 0;
+        private int Encendido = 0;
+        private int velocidad_actual = 0;
+        //private const int MAXVELOCIDAD = 149;
+        private int freno = 0;
+        public int MAXVELOCIDAD { get; }
+
+
+
+        public Carro(string _marca, int _modelo, string _color, int maxvel)
+        {
+            Marca = _marca;
+            Modelo = _modelo;
+            Color = _color;
+            Encendido = 0;
+            MAXVELOCIDAD = maxvel;
+            this.velocidad_actual = 0;
+        }
+
+
+
+        public string acelerar()
+        {
+            if (Encendido == 0)
+            {
+                return $"El carro esta apagado";
+            }
+
+
+            string mensaje = "";
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+               
+                velocidad_actual += 30;
+                mensaje = $"vas a {velocidad_actual} KPH";
+            }
+            else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = "Aguas, vas a maxima velocidad, Disminuye tu velocidad ";
+
+            }
+
+            //Console.WriteLine(mensaje);
+            return mensaje;
+
+        }
+
+
+
+        public string acelerar(int AcelerarAkph)
+        {
+            if (Encendido == 0)
+            {
+                return $"El carro esta apagado, asi no se puede";
+            }
+
+
+            string mensaje = "";
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                velocidad_actual += AcelerarAkph;
+                mensaje = $"vas a {velocidad_actual} KPH";
+            }
+            else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = "Aguas, vas a maxima velocidad, rapido y furioso???";
+
+            }
+
+            //Console.WriteLine(mensaje);
+            return mensaje;
+
+        }
+
+
+
+        public string GetVelocidadActual()
+        {
+            return $"VAmos a {velocidad_actual}KPH";
+        }
+
+
+        public string EncenderMotor()
+        {
+
+            string mensaje = "";
+
+            if (Encendido == 0)
+            {
+                Encendido = 1;
+                mensaje = "Brururururururu carro encendido!!";
+                velocidad_actual = 0;
+
+            }
+            else
+            {
+                mensaje = "ups el carro ya estaba encendido";
+            }
+            return mensaje;
+        }
+
+
+
+        public string Desacelerar()
+        {
+
+            string mensaje = "";
+
+
+            if (velocidad_actual <= 0) 
+            {
+
+                mensaje = $"has parado 0 KPH\n";
+
+            }
+            else
+            {
+                mensaje = $"disminuyes la velocidad a {velocidad_actual} KPH";
+                velocidad_actual -= 30;
+            }
+
+
+
+
+            //Console.WriteLine(mensaje);
+            return mensaje;
+
+        }
+
+        public string Frenado()
+        {
+
+            string mensaje = "";
+
+            freno = velocidad_actual;
+
+            velocidad_actual -= freno;
+            mensaje = $"\nAGUAS EL TRAFICO!! HAS FRENADO {velocidad_actual} KPH\n";
+
+
+
+            //Console.WriteLine(mensaje);
+            return mensaje;
+
+        }
+
+
+        public string pitar()
+        {
+            string mensaje = "";
+
+                mensaje = "\nHAS PITADO";
+            
+
+            return mensaje;
+        }
+
+
+
+        public string ApagarMotor()
+        {
+            string mensaje = "";
+
+            if (Encendido == 1)
+            {
+                Encendido = 0;
+                mensaje= "Has apagado el auto";
+            }
+            else
+            {
+                mensaje = "EL CARRO SIGUE PRENDIDO";
+
+            }
+            return mensaje;
+
+        }
+
+
+
+        
+
+
+    }
+}
